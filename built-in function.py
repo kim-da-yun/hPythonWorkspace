ad7a546aface133e143bf60877752766509a086c
dayun <day73299@naver.com>
2021-10-12 19:15:00 +0900

    11-7 내장함수

---
+++ built-in function.py
@@ -0,0 +1,22 @@
+# 내장함수
+
+# 내장되어있어서 import 할 필요 없이 바로 사용 가능한 함수
+
+# input : 사용자 입력을 받는 함수
+# language = input("무슨 언어를 좋아하세요? ")
+# print("{0}은(는) 아주 좋은 언어입니다.".format(language))
+
+# dir : 어떤 객체를 넘겨줬을 때 그 객체가 어떤 변수와 함수를 가지고 있는지 표시
+# print(dir())
+# import random # 외장 함수
+# print(dir())
+# import pickle
+# print(dir())
+
+# print(dir(random)) # 랜덤 모듈내에서 쓸 수 있는 함수 등을 표시
+
+# lst = [1,2,3]
+# print(dir(lst))
+
+name = "jim"
+print(dir(name))
